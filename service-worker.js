5f3a130d1ef89efa353e855b7ae0ae7dc1edf206
JangAfrica Learning Centre <info.jangafrica@gmail.com>
2026-01-28 07:51:36 +0000

    Update service worker to include manifest.json
    Added manifest.json to the cache files and simplified event listener code.

--- service-worker.js
+++ service-worker.js
@@ -1,47 +1,42 @@
 const CACHE_NAME = "tga-sms-v1";
 
 const FILES_TO_CACHE = [
-    "./",
-    "./index.html",
-    "./admin_dashboard.html",
-    "./staff_dashboard.html",
-    "./parent_dashboard.html",
-    "./student_dashboard.html",
-    "./transport-logistics.html",
-    "./logo.png"
+  "/",
+  "/index.html",
+  "/admin_dashboard.html",
+  "/staff_dashboard.html",
+  "/parent_dashboard.html",
+  "/student_dashboard.html",
+  "/transport-logistics.html",
+  "/logo.png",
+  "/manifest.json"
 ];
 
 // Install
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
-    self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
 });
 
 // Activate
 self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keyList =>
-            Promise.all(
-                keyList.map(key => {
-                    if (key !== CACHE_NAME) {
-                        return caches.delete(key);
-                    }
-                })
-            )
-        )
-    );
-    self.clients.claim();
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => key !== CACHE_NAME && caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 // Fetch
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
 });
